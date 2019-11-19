f253c0dca28aef0f70706f24b18f977cb18df95f
Oleh_M <morhaliuk@gmail.com>
2019-11-19 16:41:54 +0200

    Done

--- iteration.py
+++ iteration.py
@@ -1,6 +1,18 @@
+"""Calculates sin(x) using Taylor series"""
 import math
 
 
-def sin_(x: float) -> float:
+def ser_el(var, elem):
+    """Returns value of n-th element of a sine Teylor series"""
+    return ((-1)**elem) * (var**(2*elem + 1))/math.factorial(2*elem + 1)
 
-    return 42.
+
+def sin_(var: float) -> float:
+    """Returns sin(x) using Taylor series"""
+    num_el = 7
+    res = 0
+    for i in range(num_el):
+        res += ser_el(var, i)
+        print(res)
+
+    return res
