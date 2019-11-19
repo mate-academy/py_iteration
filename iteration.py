720714bcf186744aa8733578c57b771e65d4fd08
Artem <artger@ukr.net>
2019-11-19 15:57:25 +0200

    Complete iteration

--- iteration.py
+++ iteration.py
@@ -1,6 +1,10 @@
+'''Module'''
 import math
 
 
-def sin_(x: float) -> float:
-
-    return 42.
+def sin_(degree: float) -> float:
+    '''Result def'''
+    answer = 0
+    for i in range(10):
+        answer += ((-1) ** i) * (degree ** (2 * i + 1) / math.factorial(2 * i + 1))
+    return answer
--- test_iteration.py
+++ test_iteration.py
@@ -1,15 +1,19 @@
+'''Test module'''
 import math
 
 import iteration
 
 
 def test_0():
+    '''test1'''
     assert math.isclose(iteration.sin_(0), 0)
 
 
 def test_pi_2():
+    '''test2'''
     assert math.isclose(iteration.sin_(math.pi/2), 1)
 
 
 def test_pi_6():
+    '''test3'''
     assert math.isclose(iteration.sin_(math.pi/6), 0.5)
