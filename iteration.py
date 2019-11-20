abe6f7968f516f8255bf2ce4edaa6a96837da6bd
Platoniuks <platon@alterpost.org>
2019-11-20 18:30:28 +0200

    oh my god

--- iteration.py
+++ iteration.py
@@ -1,6 +1,11 @@
+"""Calculates sin(x)"""
 import math
 
 
-def sin_(x: float) -> float:
-
-    return 42.
+def sin_(param: float) -> float:
+    """Calculates sin(x)"""
+    result = 0
+    for i in range(10):
+        first = (-1) ** i
+        result += first * (param ** (2 * i + 1) / math.factorial(2 * i + 1))
+    return result
