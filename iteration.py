965ce40a18cbf18c71d840d6b2b6a2cd4cc58c29
dima <dima.sapsay@gmail.com>
2019-11-20 08:30:55 +0200

    py_iteration Dima Sapsai (ver 2)

--- iteration.py
+++ iteration.py
@@ -7,6 +7,6 @@ import math
 def sin_(value: float) -> float:
     """Calculate sin(x)"""
     calculate = 0
-    for num in range(10):
-        calculate += ((-1) ** num) * (value ** (2 * num + 1) / math.factorial(2 * num + 1))
+    for num in range(1, 10):
+        calculate += ((-1) ** (num - 1)) * (value ** (2 * num - 1) / math.factorial(2 * num - 1))
     return calculate
