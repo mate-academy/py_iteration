efa496644d6503979038059f0f5d8bb55ab82792
badian <e.mailbox.sbv@gmail.com>
2019-11-25 01:52:38 +0200

    first

--- iteration.py
+++ iteration.py
@@ -1,6 +1,13 @@
+"""
+TAYLOR SIN FUNCTION by .badian
+"""
 import math
 
 
-def sin_(x: float) -> float:
-
-    return 42.
+def sin_(value: float) -> float:
+    """sin function Taylor series"""
+    accuracy = 10
+    result = 0
+    for i in range(accuracy):
+        result += ((-1) ** i) * (value ** (2 * i + 1) / math.factorial(2 * i +1))
+    return result
