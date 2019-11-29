0343d7723b02bb772f931aef7f1555bd99c27ca4
koreyo <KoretsValentyn@gmail.com>
2019-11-29 13:25:24 +0200

    hw3

--- iteration.py
+++ iteration.py
@@ -1,6 +1,12 @@
+"""module sin"""
 import math
 
 
-def sin_(x: float) -> float:
-
-    return 42.
+def sin_(xarg: float) -> float:
+    """sinus taylor"""
+    res = 0.0
+    for count in range(1, 10):
+        res = res + (math.pow(-1, count-1) *
+                     ((math.pow(xarg, 2 * count - 1))/(math.factorial(2*count-1))))
+    print(res)
+    return res
